a4402545f8ca8648ceeaa30fb54a7fadfcd70f13
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2023-01-08 15:52:34 -0500

    Toggle dark-theme on site-header-theme-toggler click

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elThemeToggler = document.querySelector('.site-header-theme-toogler');
+
+elThemeToggler.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode')
+})
