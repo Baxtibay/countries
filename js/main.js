ad2d006d7a99d8d6d69790135ab48f0e3a0e1932
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-01-13 23:58:46 +0500

    change to more functional the dark-mode with localstorage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 const elThemeToggler = document.querySelector('.site-header-theme-toogler');
 
-elThemeToggler.addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode')
+if(localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode')
+}
+
+elThemeToggler.addEventListener('click', () => {
+  if(elThemeToggler.classList.contains('light')) {
+    elThemeToggler.classList.remove('light')
+    elThemeToggler.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+    if(localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark-mode')
+    }
+  } else {
+    elThemeToggler.classList.remove('dark')
+    elThemeToggler.classList.add('light')
+    localStorage.removeItem('theme', 'dark')
+    localStorage.setItem('theme', 'light')
+    if(localStorage.getItem('theme') === 'light') {
+      document.body.classList.remove('dark-mode')
+    }
+  }
 })
